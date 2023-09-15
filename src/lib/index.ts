e068d1a5794521ca20c8477e9e160272b98b3642
CNSeniorious000 <wjj123345567@icloud.com>
2023-09-16 02:29:00 +0800

    chore: update python example

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,32 +1,32 @@
 export const examplePythonScript = `
 
-from promplate.llm.openai import chat_complete
-from promplate import ChatTemplate, Node
+from promplate.llm.openai import ChatComplete
+from promplate import Node
 
-reply = Node(ChatTemplate("""
+reply = Node("""
 <| system |>
 Now is {{ time.localtime() }}
 <| user |>
 What the time is it?
-""".strip()), temperature=0.8)
+""".strip(), temperature=0.8)
 
-translate = Node(ChatTemplate("""
-<| system |>
-Translate the following message to \`ja_JP\`:
-<| user message_to_translate |>
+translate = Node("""
+Translate the following dialog to \`ja_JP\`:
 {{ name.title() }}: {{ __result__ }}
-""".strip()), temperature=0.2)
+""".strip(), temperature=0.2)
 
 chain = reply + translate
+
 # >>> print(chain)
 # </reply/> + </translate/>
 
-complete = chat_complete(model="gpt-3.5-turbo")
+complete = ChatComplete(model="gpt-3.5-turbo")
 
 def main():
     """🚀 run the chain"""
 
     import time
+
     name = "John"
 
     return chain.run(locals(), complete)["__result__"]
