8559b552e6c40d948edd9d7b996d38ac3faaaa2e
CNSeniorious000 <wjj123345567@icloud.com>
2023-10-01 05:48:19 +0800

    feat: update docs and update scrollbar style

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -42,7 +42,15 @@
     --uno: bg-white/15;
   }
 
-  :global(*)::-webkit-scrollbar {
+  :global(body)::-webkit-scrollbar {
+    --uno: bg-neutral-9 w-1 sm:w-1.5 md:w-2 lg:w-2.5 xl:w-3 2xl:w-3.5;
+  }
+
+  :global(body)::-webkit-scrollbar-thumb {
+    --uno: bg-neutral-7/30 hover:bg-neutral-7/70 rounded-l-sm;
+  }
+
+  :global(body *)::-webkit-scrollbar {
     --uno: hidden;
   }
 </style>
