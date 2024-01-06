644c99523d1cd3c2fd4bc53eaec45e887178ae98
Muspi Merol <wjj123345567@icloud.com>
2024-01-06 17:27:17 +0800

    feat: sync with latest `promplate` features

---
+++ src/lib/examples/show-time-and-translate.py
@@ -0,0 +1,26 @@
+import time
+
+from promplate import Node
+from promplate.llm.openai import ChatComplete
+
+reply_template = """
+<| system |>
+current time: {{ time.localtime() }}
+<| user |>
+Say happy new year to me in no more than 5 words.
+Note that you must include the year in the message.
+"""
+
+translate_template = """
+Translate the following dialog to `ja_JP`:
+{{ name.title() }}: {{ __result__ }}
+"""
+
+reply = Node(reply_template, temperature=0.8)
+translate = Node(translate_template, temperature=0.2)
+
+
+output = (reply + translate).invoke(
+    {"time": time, "name": "muspi merol"},
+    ChatComplete().bind(model="gpt-3.5-turbo"),
+)
