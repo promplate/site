38516b6b6173c075f9693c8cca435d6e60d20b26
Muspi Merol <wjj123345567@icloud.com>
2024-02-24 20:22:08 +0800

    feat: more native Chinese examples

--- src/lib/examples/show-time-and-translate.py
+++ src/lib/examples/show-time-and-translate.py
@@ -3,23 +3,28 @@ import time
 from promplate import Node
 from promplate.llm.openai import ChatComplete
 
+# 用日语撰写一段祝福
 reply_template = """
 <| system |>
 current time: {{ time.ctime() }}
 <| user |>
-Say happy new year to me in no more than 5 words.
+Say happy new year to me in native Japanese.
 Note that you must include the year in the message.
 """
 
+# 翻译成中文
 translate_template = """
-Translate the following dialog to `ja_JP`:
+Translate the following dialog to `zh_CN`:
 {{ name.title() }}: {{ __result__ }}
 """
 
 reply = Node(reply_template, temperature=0.8)
 translate = Node(translate_template, temperature=0.2)
 
-output = (reply + translate).invoke(
+# 获得一段日系祝福
+chain = reply + translate
+
+output = chain.invoke(
     {"time": time, "name": "muspi merol"},
     ChatComplete().bind(model="gpt-3.5-turbo"),
-)
+)  # 大约等待 3 秒，这期间会请求两次 OpenAI
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,6 +3,6 @@ import examplePythonScript from "./examples/show-time-and-translate.py?raw";
 export { examplePythonScript };
 
 export const examplePythonOutput = `
->>> output.result  # wait for about 5s
-'Muspi Merol: あけましておめでとうございます！2024年です！'
+>>> output.result
+'穆斯皮·梅罗：新年快乐！2024年也请多多关照。'
 `.trim();
