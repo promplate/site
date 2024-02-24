f33bff5002587264e0e2d373e9c6a58d8849c45a
Muspi Merol <wjj123345567@icloud.com>
2024-02-24 11:35:18 +0800

    feat: self-host `pyodide` assets

--- src/lib/pyodide/init.ts
+++ src/lib/pyodide/init.ts
@@ -1,14 +1,13 @@
 import * as env from "$env/static/public";
 import { cacheSingleton } from "$lib/utils/cache";
 import { withToast } from "$lib/utils/toast";
-import { version } from "pyodide/package.json";
 import { toast } from "svelte-sonner";
 
-const indexURL = `https://cdn.jsdelivr.net/pyodide/v${version}/full/`;
+const indexURL = typeof window === "undefined" ? undefined : (process.env.NODE_ENV === "production" && env.PUBLIC_PYODIDE_INDEX_URL) || "/pyodide/";
 
 async function initPyodide() {
   const { loadPyodide } = await import("pyodide");
-  return await loadPyodide({ indexURL, env: { ...env } });
+  return await loadPyodide({ indexURL, env: { ...env }, packages: ["micropip", "typing-extensions"] });
 }
 
 async function initPy() {
@@ -18,12 +17,11 @@ async function initPy() {
     import("openai/version"),
     import("./init.py?raw"),
   ]);
-  const info = toast.loading("installing python dependencies");
+  const info = toast.loading("installing extra python dependencies");
 
   py.registerJsModule("openai", { AsyncClient, Client: () => null, version, __all__: [] });
   py.registerJsModule("httpx", { AsyncClient: () => null, Client: () => null });
 
-  await py.loadPackage("micropip");
   await py.runPythonAsync(initCode);
 
   toast.dismiss(info);
@@ -31,7 +29,7 @@ async function initPy() {
   return py;
 }
 
-export const getPy = cacheSingleton(withToast(initPy, { loading: "preparing pyodide runtime", success: `successfully loaded pyodide v${version}` }));
+export const getPy = cacheSingleton(withToast(initPy, { loading: "preparing pyodide runtime", success: `successfully initialized pyodide runtime` }));
 
 export async function initConsole() {
   const [py, { default: initConsoleCode }] = await Promise.all([getPy(), import("./console.py?raw")]);
