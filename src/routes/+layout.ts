8752d853a3d1718d928285e03ab845725c8fd87f
Muspi Merol <wjj123345567@icloud.com>
2024-03-15 16:32:19 +0800

    feat: use `zeabur` adapter

--- src/routes/+layout.ts
+++
@@ -1,6 +0,0 @@
-import { inject } from "@vercel/analytics";
-import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
-import { dev } from "$app/environment";
-
-inject({ mode: dev ? "development" : "production" });
-injectSpeedInsights();
