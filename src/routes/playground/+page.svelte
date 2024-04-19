04539a7577fb95ab15de6975a69cdcd381f238ae
Muspi Merol <wjj123345567@icloud.com>
2024-04-19 23:38:31 +0800

    feat: support multi-line paste and cross-line backspace

--- src/routes/playground/+page.svelte
+++ src/routes/playground/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
   import type { ActionData } from "./$types";
-  import type { AutoComplete, Item } from "$lib/components/console/HeadlessConsole.svelte";
+  import type { AutoComplete, Item, Status } from "$lib/components/console/HeadlessConsole.svelte";
   import type { Source } from "$lib/utils/source";
   import type { PyProxy } from "pyodide/ffi";
-  import type { KeyboardEventHandler } from "svelte/elements";
+  import type { ClipboardEventHandler, KeyboardEventHandler } from "svelte/elements";
 
   import { page } from "$app/stores";
   import { Err, In, Out, Repr } from "$lib/components/console";
@@ -30,6 +30,8 @@
 
   let pyConsole: PyProxy;
   let complete: AutoComplete;
+
+  let status: Status;
 
   let focusedError: { traceback: string; code: string };
 
@@ -129,6 +131,28 @@
       case "Enter": {
         handleInput();
         index = -1;
+
+        break;
+      }
+
+      case "Backspace": {
+        if (inputRef.selectionStart === 0 && inputRef.selectionEnd === 0 && status === "incomplete") {
+          const item = log.at(-1)!;
+          const lines = item.text.split("\n");
+          pyConsole.buffer.pop();
+          if (lines.length === 1) {
+            log = [...log.slice(0, -1)];
+            input = lines[0] + input;
+            status = "complete";
+          }
+          else {
+            const lastLine = lines.pop()!;
+            log = [...log.slice(0, -1), { type: "in", text: lines.join("\n") }];
+            input = lastLine + input;
+          }
+          history.pop();
+          event.preventDefault();
+        }
         break;
       }
 
@@ -137,11 +161,26 @@
       }
     }
   };
+
+  const onPaste: ClipboardEventHandler<HTMLInputElement> = async (event) => {
+    event.preventDefault();
+    const text = (event.clipboardData?.getData("text/plain") ?? await navigator.clipboard.readText());
+
+    const lines = text.replaceAll("\r\n", "\n").split("\n");
+
+    while (lines.length > 1) {
+      input += lines.shift()!;
+      await push(input);
+      input = "";
+    }
+    input += lines[0];
+    setCusorToEnd();
+  };
 </script>
 
 <div class="my-4 w-[calc(100vw-2rem)] flex flex-row gap-4 p-3 text-neutral-3 <lg:(my-3 w-[calc(100vw-1.5rem)] gap-3 p-2 text-sm) <sm:(my-2 w-[calc(100vw-1rem)] gap-2 p-1 text-xs) [&>div]:(overflow-x-scroll rounded bg-white/3 p-5 <lg:p-4 <sm:p-3)">
   <div class="w-full flex flex-col gap-0.7 whitespace-pre-wrap font-mono [&>div:hover]:(rounded-sm bg-white/2 px-1.7 py-0.6 -mx-1.7 -my-0.6)">
-    <HeadlessConsole bind:ready bind:log bind:push bind:pushLog bind:complete bind:pyConsole let:status let:loading>
+    <HeadlessConsole bind:ready bind:log bind:push bind:pushLog bind:complete bind:pyConsole bind:status let:loading>
       {#each log as { type, text }, index}
         {#if type === "out"}
           <Out {text} />
@@ -156,7 +195,7 @@
       <div class="group flex flex-row" class:animate-pulse={loading || !ready}>
         <ConsolePrompt prompt={status === "incomplete" ? "..." : ">>>"} />
         <!-- svelte-ignore a11y-autofocus -->
-        <input autofocus bind:this={inputRef} class="w-full bg-transparent outline-none" bind:value={input} type="text" on:keydown={onKeyDown} />
+        <input autofocus bind:this={inputRef} class="w-full bg-transparent outline-none" bind:value={input} type="text" on:keydown={onKeyDown} on:paste={onPaste} />
       </div>
     </HeadlessConsole>
   </div>
