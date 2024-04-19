57746142d2441156230809d34e5f48de92be0a70
Muspi Merol <wjj123345567@icloud.com>
2024-04-19 19:31:49 +0800

    chore: update `promplate` and `promplate-pyodide`

--- src/lib/pyodide/init.py
+++ src/lib/pyodide/init.py
@@ -4,8 +4,8 @@ from micropip import install
 
 await install(
     [
-        "promplate==0.3.3.4",
-        "promplate-pyodide==0.0.2.2",
+        "promplate==0.3.4.2",
+        "promplate-pyodide==0.0.3.2",
     ]
 )
 del install
